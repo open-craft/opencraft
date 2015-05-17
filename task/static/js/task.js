c829389da4a19141a560d3b6aa5de3a4cb63fb00
Xavier Antoviaque <xavier@antoviaque.org>
2015-05-17 13:51:42 +0200

    Load task list from API via Angular

--- task/static/js/task.js
+++ task/static/js/task.js
@@ -4,26 +4,35 @@ var app = angular.module('TaskApp', [
     'restangular'
 ]);
 
-app.config(function ($stateProvider, $urlRouterProvider, RestangularProvider) {
+app.config(function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+});
+
+app.config(function($stateProvider, $urlRouterProvider, RestangularProvider) {
     // For any unmatched url, send to /
     $urlRouterProvider.otherwise("/task");
 
     $stateProvider
         .state('index', {
             url: "",
-            templateUrl: "/static/html/partials/_task_list.html",
+            templateUrl: "/static/html/task_list.html",
             controller: "TaskList"
         })
-        .state('new', {
-            url: "new",
-            templateUrl: "/task/task-form",
-            controller: "TaskFormCtrl"
-        });
 });
 
-app.controller("TaskFormCtrl", ['$scope', 'Restangular', 'CbgenRestangular', '$q',
-    function ($scope, Restangular, CbgenRestangular, $q) {
+app.factory('OpenCraftAPI', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfigurer) {
+        RestangularConfigurer.setBaseUrl('/api/v1');
+    });
+});
 
-
+app.controller("TaskList", ['$scope', 'Restangular', 'OpenCraftAPI', '$q',
+    function ($scope, Restangular, OpenCraftAPI, $q) {
+        
+        OpenCraftAPI.all("task/").getList().then(function(taskList) {
+            $scope.taskList = taskList;
+        }, function(response) {
+            console.log('Error from server: ', response);
+        });
     }
 ]);
