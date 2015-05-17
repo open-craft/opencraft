7f8fd5ecfc0acaa9670e1a273683eb14a2d00aad
Xavier Antoviaque <xavier@antoviaque.org>
2015-05-18 01:00:52 +0200

    Add favicon

--- opencraft/urls.py
+++ opencraft/urls.py
@@ -2,6 +2,7 @@
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls', namespace="api")),
     url(r'^task/', include('task.urls', namespace="task")),
-    url(r'^', 'task.views.index'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon/favicon.ico')),
+    url(r'^$', 'task.views.index'),
 ]
