47fb142f285f989f7b911915b7b130bf4a72254b
Xavier Antoviaque <xavier@antoviaque.org>
2015-05-20 22:18:43 +0200

    Remove 1.8 warning about redirect

--- opencraft/urls.py
+++ opencraft/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls', namespace="api")),
     url(r'^task/', include('task.urls', namespace="task")),
-    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon/favicon.ico')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon/favicon.ico', permanent=False)),
     url(r'^$', 'task.views.index'),
 ]
