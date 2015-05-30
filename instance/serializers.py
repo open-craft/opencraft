15492594186b6a0dcea510e6896310f5e45368fc
Xavier Antoviaque <xavier@antoviaque.org>
2015-05-30 11:52:53 +0200

    API: Include both the numerical instance.pk & its API URL

--- instance/serializers.py
+++ instance/serializers.py
@@ -9,20 +9,20 @@ from rest_framework import serializers
 from .models import OpenStackServer, OpenEdXInstance
 
 class OpenStackServerSerializer(serializers.ModelSerializer):
-    pk = serializers.HyperlinkedIdentityField(view_name='api:openstackserver-detail')
+    pk_url = serializers.HyperlinkedIdentityField(view_name='api:openstackserver-detail')
     instance = serializers.HyperlinkedRelatedField(view_name='api:openedxinstance-detail', read_only=True)
 
     class Meta:
         model = OpenStackServer
-        fields = ('pk', 'status', 'instance', 'openstack_id', 'created', 'modified')
+        fields = ('pk', 'pk_url', 'status', 'instance', 'openstack_id', 'created', 'modified')
 
 class OpenEdXInstanceSerializer(serializers.ModelSerializer):
-    pk = serializers.HyperlinkedIdentityField(view_name='api:openedxinstance-detail')
+    pk_url = serializers.HyperlinkedIdentityField(view_name='api:openedxinstance-detail')
     server_set = OpenStackServerSerializer(many=True, read_only=True)
 
     class Meta:
         model = OpenEdXInstance
-        fields = ('pk', 'server_set', 'sub_domain', 'base_domain', 'email', 'name', 'protocol',
+        fields = ('pk', 'pk_url', 'server_set', 'sub_domain', 'base_domain', 'email', 'name', 'protocol',
                   'domain', 'url', 'branch_name', 'commit_id', 'github_organization_name',
                   'github_organization_name', 'github_base_url', 'repository_url', 'updates_feed',
                   'vars_str', 'created', 'modified')
