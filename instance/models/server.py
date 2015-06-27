fa162d8cd143b6628f0883efa6d51f55c21ebeae
Xavier Antoviaque <xavier@antoviaque.org>
2015-06-27 11:04:25 +0200

    Add licensing information & headers to Python files

--- instance/models/server.py
+++ instance/models/server.py
@@ -1,8 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# OpenCraft -- tools to aid developing and hosting free software projects
+# Copyright (C) 2015 OpenCraft <xavier@opencraft.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 """
 Instance app models - Server
 """
-#pylint: disable=no-init
-
 
 # Imports #####################################################################
 
@@ -128,9 +144,8 @@ class OpenStackServer(Server):
     def update_status(self):
         '''
         Refresh the status by querying the openstack server via nova
-
-        TODO: Check when server is stopped or terminated
         '''
+        # TODO: Check when server is stopped or terminated
         # Ensure the 'started' mode by getting the server instance from openstack
         os_server = self.os_server
         self.log('debug', 'Updating status for {} from nova (currently {}):\n{}'\
--- instance/routers.py
+++ instance/routers.py
@@ -1,6 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# OpenCraft -- tools to aid developing and hosting free software projects
+# Copyright (C) 2015 OpenCraft <xavier@opencraft.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+SwampDragon routers - Register channels available to the client-side (browser)
+"""
+
+# Imports #####################################################################
+
 from swampdragon import route_handler
 from swampdragon.route_handler import BaseRouter
 
+
+# Routers #####################################################################
 
 class NotificationRouter(BaseRouter): #pylint: disable=abstract-method
     route_name = 'notifier'
@@ -8,5 +34,7 @@ class NotificationRouter(BaseRouter): #pylint: disable=abstract-method
     def get_subscription_channels(self, **kwargs):
         return ['notification', 'log']
 
+
+# Routers registration ########################################################
 
 route_handler.register(NotificationRouter)
