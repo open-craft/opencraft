279d1700a13045b37c9e694eef7f4d6d133c1f77
Xavier Antoviaque <xavier@antoviaque.org>
2015-08-06 18:56:08 +0200

    Update log message format - Include process number

--- opencraft/settings.py
+++ opencraft/settings.py
@@ -289,7 +289,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'format': "[{asctime}] {levelname:>8s} | process={process:<5d} | {name} | {message}",
+            'style': '{',
             'datefmt': "%d/%b/%Y %H:%M:%S"
         },
         'simple': {
