51ac0127218189e1145f32c7cda6d3c84894cfb7
Sven Marnach <sven@marnach.net>
2016-10-23 20:29:56 +0200

    Test that accessing the instance via the load balancer works.

---
+++ instance/tests/integration/utils.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# OpenCraft -- tools to aid developing and hosting free software projects
+# Copyright (C) 2015-2016 OpenCraft <contact@opencraft.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Integration tests - helper functions.
+"""
+
+import time
+
+import requests
+
+
+def check_url_accessible(url, attempts=3, delay=15):
+    """
+    Check that the given URL is accessible and returns a success status code.
+
+    Raises an exception if there is an HTTP error.
+    """
+    while True:
+        attempts -= 1
+        try:
+            requests.get(url).raise_for_status()
+            break
+        except Exception:  # pylint: disable=broad-except
+            if not attempts:
+                raise
+        time.sleep(delay)
