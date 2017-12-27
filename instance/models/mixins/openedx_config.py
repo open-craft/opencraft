a205d25619d70bd0dc0ad5f0b275d6228f94a15a
Daniel Clemente <daniel@opencraft.com>
2017-12-27 21:49:42 +0200

    Add setting to avoid memory leak, and add comment about number of workers (#256)

--- instance/models/mixins/openedx_config.py
+++ instance/models/mixins/openedx_config.py
@@ -198,10 +198,19 @@ class OpenEdXConfigMixin(models.Model):
             },
 
             # Gunicorn workers
+            # By default, the number of workers is num_cores*4 for LMS, num_cores*2 for CMS,
+            # which turns out to be too much.
             "EDXAPP_WORKERS": {
                 "lms": 3,
                 "cms": 2,
             },
+
+            # Restart workers regularly to work around a memory leak
+            # Tailor the max_requests number to suit the average request load on the server.
+            # e.g if the instance receives an average of around 42000 requests per day,
+            # with 10% for static assets, restarting after 20000 requests means
+            # restarting each of the 4 LMS workers about every 2-3 days.
+            "EDXAPP_LMS_MAX_REQ": 20000,
 
             # Celery workers
             "EDXAPP_WORKER_DEFAULT_STOPWAITSECS": 1200,
