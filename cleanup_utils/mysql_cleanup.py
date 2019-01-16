0c919a75e38d180f006d8d15fde672cd74224e2d
James Tait <james@opencraft.com>
2019-01-16 19:22:49 +0000

    Parse the DB URL, connect, and create a cursor. Add somewhere to store hashes of removed DBs.

--- cleanup_utils/mysql_cleanup.py
+++ cleanup_utils/mysql_cleanup.py
@@ -23,6 +23,10 @@ Cleans up all MySQL databases left behind from CI
 """
 
 import logging
+from urllib.parse import urlparse
+
+import MySQLdb as mysql
+from MySQLdb import Error as MySQLError
 
 
 # Logging ####################################################################
@@ -41,8 +45,26 @@ class MySqlCleanupInstance:
         Set up variables needed for cleanup
         """
         self.age_limit = age_limit
+        self.cleaned_up_hashes = []
         self.dry_run = dry_run
-        self.url = url
+        self.cursor = self._get_cursor(url)
+
+    def _get_cursor(self, url):
+        """
+        Returns a cursor on the database
+        """
+        database_url_obj = urlparse(url)
+        try:
+            connection = mysql.connect(
+                host=database_url_obj.hostname,
+                user=database_url_obj.username or '',
+                passwd=database_url_obj.password or '',
+                port=database_url_obj.port or 3306,
+            )
+        except MySQLError as exc:
+            logger.excecption('Cannot get MySQL cursor: %s', exc)
+            raise
+        return connection.cursor()
 
     def run_cleanup(self):
         """
@@ -51,3 +73,8 @@ class MySqlCleanupInstance:
         logger.info("\n --- Starting MySQL Cleanup ---")
         if self.dry_run:
             logger.info("Running in DRY_RUN mode, no actions will be taken.")
+
+        # mysql> SELECT table_schema, MAX(create_time) AS create_time,
+        # MAX(update_time) AS update_time FROM information_schema.tables WHERE
+        # create_time <= DATE_SUB(NOW(), INTERVAL 3 DAY) GROUP BY table_schema
+        # ORDER BY create_time DESC;
