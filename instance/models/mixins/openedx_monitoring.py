c92cdf3e2459ebe4db23ee2992b60cb1eddbfd99
Loic Dachary <loic@dachary.org>
2019-03-10 10:42:14 +0100

    pylint: define invalid-name conventions
    * set the min length for attr to 2 characters (instead of 3)
    
    * set the min length for arguments to 1 character (instead of 2)
    
    * set the max length of method names to 70 characters (instead of 45)
    
    * set exceptions for the maxDiff attribute, setUp, tearDown
      etc. methods used by unittest

--- instance/models/mixins/openedx_monitoring.py
+++ instance/models/mixins/openedx_monitoring.py
@@ -92,7 +92,7 @@ class NewRelicAvailabilityMonitor(models.Model):
     """
     A New Relic Synthetics availability monitor for an instance.
     """
-    id = models.CharField(max_length=256, primary_key=True)  # pylint: disable=invalid-name
+    id = models.CharField(max_length=256, primary_key=True)
     instance = models.ForeignKey(
         'OpenEdXInstance', related_name='new_relic_availability_monitors', on_delete=models.CASCADE
     )
