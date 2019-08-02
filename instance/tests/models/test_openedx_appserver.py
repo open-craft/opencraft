2000b0d5a2c1a272b88143a17bcda6545a2df19e
Daniel Clemente Laboreo <daniel@opencraft.com>
2019-08-03 00:43:13 +0200

    typo (hearbeat → heartbeat)

--- instance/tests/models/test_openedx_appserver.py
+++ instance/tests/models/test_openedx_appserver.py
@@ -852,15 +852,15 @@ class EmailMixinInstanceTestCase(TestCase):
         self.assertEqual(mime_type, "text/html")
 
     @responses.activate
-    def test_hearbeat_active_succeeds(self):
-        """ Test that heartbeat_active method returns true when request to hearbeat is 200"""
+    def test_heartbeat_active_succeeds(self):
+        """ Test that heartbeat_active method returns true when request to heartbeat is 200"""
         appserver = make_test_appserver()
         responses.add(responses.OPTIONS, 'http://{}/heartbeat'.format(appserver.server.public_ip), status=200)
         self.assertTrue(appserver.heartbeat_active())
 
     @patch('requests.options')
     def test_heartbeat_active_fails(self, mock_requests_options):
-        """ Test that heartbeat_active method returns false when request to hearbeat fails"""
+        """ Test that heartbeat_active method returns false when request to heartbeat fails"""
         mock_requests_options.side_effect = requests.exceptions.ConnectionError()
         appserver = make_test_appserver()
         self.assertFalse(appserver.heartbeat_active())
