8c4679b745827a6d352d29fde56eec7397977e39
Guruprasad Lakshmi Narayanan <guruprasad@opencraft.com>
2019-09-06 20:01:54 +0530

    Fix typo

--- instance/media_storage.py
+++ instance/media_storage.py
@@ -21,7 +21,7 @@ Media storage classes
 """
 
 from django.conf import settings
-from storages.backends.s3bot3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class S3MediaStorage(S3Boto3Storage):
