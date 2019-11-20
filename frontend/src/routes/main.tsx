6f48fa5ec85293069c213a5f5e8d0e5198e14261
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-11-20 15:37:42 -0300

    Drop home component and add registration

--- frontend/src/routes/main.tsx
+++ frontend/src/routes/main.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 
-import { Route, Switch } from 'react-router';
-import { ROUTES } from '../global/constants';
-import { RegistrationContainer } from '../registration/components';
-import { Home } from '../ui/components/Home';
+import { Redirect, Route, Switch } from "react-router";
+import { ROUTES } from "../global/constants";
+import { RegistrationContainer } from "../registration/components";
 
 export const MainRoutes = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
-    <Route path={ROUTES.Registration.HOME} component={RegistrationContainer} />
+    <Redirect from="/" to={ROUTES.Registration.HOME} exact/>
+    <Route path={ROUTES.Registration.HOME} component={RegistrationContainer}/>
   </Switch>
 );
--- frontend/src/ui/components/index.ts
+++ frontend/src/ui/components/index.ts
@@ -2,6 +2,4 @@ export * from './App';
 export * from './Footer';
 export * from './Header';
 export * from './Main';
-export * from './Home';
-
 export * from './StepBar';
