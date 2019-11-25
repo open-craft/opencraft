100daa1718089e23f57eec8eacfab304fefe4277
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-11-25 19:13:33 -0300

    Removing console error

--- frontend/src/ui/components/StepBar/StepBar.tsx
+++ frontend/src/ui/components/StepBar/StepBar.tsx
@@ -47,10 +47,10 @@ export const StepBar: React.FC<StepBarProps> = ({
 }: StepBarProps) => (
   <div className="step-bar">
     {[...Array(count).keys()].map(num => (
-      <>
+      <React.Fragment key={"registration_stepbar_"+num}>
         {num + 1 === currentStep ? <CircledNumber number={num + 1} /> : circle}
         <span className="line" />
-      </>
+      </React.Fragment>
     ))}
   </div>
 );
