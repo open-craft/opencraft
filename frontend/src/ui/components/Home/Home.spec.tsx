19c6cd3f396cc5d58dc3fd15bcc57984a308411f
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-11-25 18:44:10 -0300

    Remove unused component

--- frontend/src/ui/components/Home/Home.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { setupComponentForTesting } from 'utils/testing';
-import { Home } from './Home';
-
-it('renders without crashing', () => {
-  const tree = setupComponentForTesting(<Home />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
