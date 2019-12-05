63adba0ff94edc511ce536bf8cea2c7c80fc209f
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2019-12-05 16:39:49 -0300

    Update snapshots and fix tests

--- frontend/src/ui/components/TextInputField/TextInputField.spec.tsx
+++ frontend/src/ui/components/TextInputField/TextInputField.spec.tsx
@@ -2,7 +2,24 @@ import React from 'react';
 import { setupComponentForTesting } from "utils/testing";
 import { TextInputField } from './TextInputField';
 
+const messages = {
+  test: {
+    defaultMessage: 'A translatable string.',
+    description: 'A description of the translatable string.'
+  }
+};
+
+
 it('renders without crashing', () => {
-    const tree = setupComponentForTesting(<TextInputField />).toJSON();
+    const tree = setupComponentForTesting(
+      <TextInputField
+        fieldName="test"
+        value="test"
+        type="password"
+        error="test"
+        onChange={() => {}}
+        messages={messages}
+      />
+    ).toJSON();
     expect(tree).toMatchSnapshot();
 });
--- frontend/src/ui/components/TextInputField/displayMessages.ts
+++
@@ -1,8 +0,0 @@
-const messages = {
-  messageID: {
-    defaultMessage: 'A translatable string.',
-    description: 'A description of the translatable string.'
-  }
-};
-
-export default messages;
