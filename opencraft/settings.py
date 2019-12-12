5b59c1310d939ac572f2504ec0998f5f041dc3c6
Kshitij Sobti <kshitij@sobti.in>
2019-12-12 18:49:26 +0530

    Quality fix2

--- opencraft/settings.py
+++ opencraft/settings.py
@@ -102,23 +102,23 @@ LOCAL_APPS = (
 )
 
 INSTALLED_APPS = (
-                     'grappelli',
-                     'django.contrib.admin',
-                     'django.contrib.auth',
-                     'django.contrib.contenttypes',
-                     'django.contrib.sessions',
-                     'django.contrib.messages',
-                     'django.contrib.staticfiles',
-                     'django_extensions',
-                     'foundation',
-                     'compressor',
-                     'djng',
-                     'rest_framework',
-                     'huey.contrib.djhuey',
-                     'simple_email_confirmation',
-                     'channels',
-                     'drf_yasg',
-                 ) + LOCAL_APPS
+    'grappelli',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_extensions',
+    'foundation',
+    'compressor',
+    'djng',
+    'rest_framework',
+    'huey.contrib.djhuey',
+    'simple_email_confirmation',
+    'channels',
+    'drf_yasg',
+) + LOCAL_APPS
 
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
