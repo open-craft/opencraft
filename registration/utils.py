c06bc3ae1bf93574b5e65ecb1cdc07ff70960bd6
Kshitij Sobti <kshitij@opencraft.com>
2019-12-13 07:29:40 +0000

    [BB-1873] Add v2 of account and instance config APIs (#499)

---
+++ registration/utils.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# OpenCraft -- tools to aid developing and hosting free software projects
+# Copyright (C) 2015-2019 OpenCraft <xavier@opencraft.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Utility functions related to registration.
+"""
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+from simple_email_confirmation.models import EmailAddress
+
+from email_verification import send_email_verification
+
+
+def verify_user_emails(user: User, request: HttpRequest, *email_addresses: str):
+    """
+    Start email verification process for specified email addresses.
+
+    This should ignore already-verified email addresses.
+    """
+    for email_address in email_addresses:
+        if not EmailAddress.objects.filter(email=email_address).exists():
+            email = EmailAddress.objects.create_unconfirmed(email_address, user)
+            send_email_verification(email, request)
