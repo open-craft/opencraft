d9b25f70f78804d2ecbdc95036e59e5787e82d29
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-01-24 14:30:51 -0300

    Add tests to console page

--- frontend/src/console/components/ConsolePage/ConsolePage.spec.tsx
+++ frontend/src/console/components/ConsolePage/ConsolePage.spec.tsx
@@ -2,7 +2,39 @@ import React from 'react';
 import { setupComponentForTesting } from "utils/testing";
 import { ConsolePage } from './ConsolePage';
 
-it('renders without crashing', () => {
-    const tree = setupComponentForTesting(<ConsolePage />).toJSON();
-    expect(tree).toMatchSnapshot();
+
+describe("Console Page", function() {
+  it('Correctly renders loading page', () => {
+      const tree = setupComponentForTesting(
+        <ConsolePage contentLoading={true}>
+          <span> Test! </span>
+        </ConsolePage>,
+        {
+          console: {
+            loading: true
+          }
+        }
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+
+  it('Correctly renders page with data', () => {
+      const tree = setupComponentForTesting(
+        <ConsolePage contentLoading={false}>
+          <span> Test! </span>
+        </ConsolePage>,
+        {
+          console: {
+            loading: false,
+            selectedInstance: 0,
+            instances: [{
+              id: 1,
+              instanceName: "test",
+              subdomain: "test",
+            }]
+          }
+        }
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
 });
