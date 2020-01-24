4a96517c20590d70368318c960e613672aa58950
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-01-24 12:07:03 -0300

    Connecting instance page to redux

--- frontend/src/console/components/InstanceSettings/InstanceSettings.tsx
+++ frontend/src/console/components/InstanceSettings/InstanceSettings.tsx
@@ -3,31 +3,45 @@ import messages from './displayMessages';
 import './styles.scss';
 import { ConsolePage } from 'console/components';
 import { TextInputField } from 'ui/components';
+import { InstancesModel } from 'console/models';
+import { connect } from 'react-redux';
+import { RootState } from 'global/state';
 
 
-export const InstanceSettings: React.FC = () => {
-  let loading = true;
+interface ActionProps {}
+interface StateProps extends InstancesModel {}
+interface Props extends StateProps, ActionProps {}
 
-  return (
-    <ConsolePage contentLoading={loading}>
-      <h2>Instance settings</h2>
+export class InstanceSettingsComponent extends React.PureComponent<Props> {
+  public render() {
+    return (
+      <ConsolePage contentLoading={this.props.loading}>
+        <h2>Instance settings</h2>
 
-      <TextInputField
-        fieldName="instanceName"
-        value={""}
-        onChange={() => {}}
-        messages={messages}
-        error={""}
-      />
+        <TextInputField
+          fieldName="instanceName"
+          value={""}
+          onChange={() => {}}
+          messages={messages}
+          error={""}
+        />
 
-      <TextInputField
-        fieldName="publicContactEmail"
-        value={""}
-        onChange={() => {}}
-        messages={messages}
-        type="email"
-        error={""}
-      />
-    </ConsolePage>
-  );
+        <TextInputField
+          fieldName="publicContactEmail"
+          value={""}
+          onChange={() => {}}
+          messages={messages}
+          type="email"
+          error={""}
+        />
+      </ConsolePage>
+    );
+  };
 };
+
+export const InstanceSettings = connect<StateProps, ActionProps, {}, Props, RootState>(
+  (state: RootState) => ({
+    ...state.console
+  }),
+  {}
+)(InstanceSettingsComponent);
