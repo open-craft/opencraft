145bb61bf595b1b69367e98b72408b078678b76d
Guruprasad Lakshmi Narayanan <guruprasad@opencraft.com>
2020-01-28 22:41:17 +0530

    Remove the code corresponding to the Gandi v4 API

--- instance/tests/test_gandi.py
+++ instance/tests/test_gandi.py
@@ -23,154 +23,15 @@ Gandi - Tests
 # Imports #####################################################################
 
 from unittest.mock import call, MagicMock, patch
-import xmlrpc.client
 
 from django.conf import settings
 
 from instance import gandi
 from instance.gandi import GandiV5API
 from instance.tests.base import TestCase
-from instance.tests.fake_gandi_client import FakeGandiClient
 
 
 # Tests #######################################################################
-
-class GandiTestCase(TestCase):
-    """
-    Test cases for Gandi API calls
-    """
-    def setUp(self):
-        super().setUp()
-        self.api = gandi.GandiAPI(client=FakeGandiClient())
-
-    def populate_cache(self):
-        """
-        Populate the zone ID cache of the Gandi API and reset the mocks.
-        """
-        self.api._populate_zone_id_cache()
-        self.api.client.domain.reset_mock()
-
-    def assert_set_dns_record_calls(self, attempts=1):
-        """
-        Verify Gandi API calls for setting a DNS record value.
-        """
-        create_new_zone_version_call = call.zone.version.new('TEST_GANDI_API_KEY', 9900)
-        delete_old_record_call = call.zone.record.delete(
-            'TEST_GANDI_API_KEY', 9900, 1,
-            {'type': ['A', 'CNAME'], 'name': 'sub.domain'}
-        )
-        create_new_record_call = call.zone.record.add(
-            'TEST_GANDI_API_KEY', 9900, 1,
-            {'value': '192.168.99.99', 'ttl': 1200, 'type': 'A', 'name': 'sub.domain'}
-        )
-        set_new_zone_version_call = call.zone.version.set('TEST_GANDI_API_KEY', 9900, 1)
-
-        self.assertEqual(
-            self.api.client.domain.mock_calls,
-            [create_new_zone_version_call] * attempts +
-            [delete_old_record_call, create_new_record_call, set_new_zone_version_call]
-        )
-
-    def test_populate_zone_id_cache(self):
-        """
-        Test that populating the zone ID cache results in the expected API calls.
-        """
-        self.api.get_zone_id('test.com')  # Implicitly calls _populate_zone_id_cache()
-        self.assertEqual(len(self.api.client.domain.mock_calls), 4)
-        self.api.client.domain.assert_has_calls([
-            call.list('TEST_GANDI_API_KEY'),
-            call.info('TEST_GANDI_API_KEY', 'test.com'),
-            call.info('TEST_GANDI_API_KEY', 'example.com'),
-            call.info('TEST_GANDI_API_KEY', 'opencraft.co.uk'),
-        ], any_order=True)
-
-    def test_split_domain_name(self):
-        """
-        Test that splitting domain names in subdomain and registered domain works correctly.
-        """
-        self.assertEqual(self.api.split_domain_name('sub.domain.test.com'), ('sub.domain', 'test.com'))
-        self.assertEqual(self.api.split_domain_name('sub.domain.opencraft.co.uk'), ('sub.domain', 'opencraft.co.uk'))
-        self.assertEqual(self.api.split_domain_name('example.com'), ('@', 'example.com'))
-        with self.assertRaises(ValueError) as error:
-            self.api.split_domain_name('sub.domain.unknown.com')
-        self.assertEqual(
-            str(error.exception),
-            'The given domain name "sub.domain.unknown.com" does not match any domain registered in the Gandi account.'
-        )
-
-    def test_get_zone_id(self):
-        """
-        Gets zone_id for the requested FQDN.
-        The zone_id is cached in memory after retreived for the first time.
-        """
-        self.populate_cache()
-        zone_id = self.api.get_zone_id('test.com')
-        self.assertEqual(zone_id, 9900)
-        # There shouldn't be any API calls because the IDs are retrieved from the cache.
-        self.assertEqual(len(self.api.client.domain.mock_calls), 0)
-
-    def test_set_dns_record(self):
-        """
-        Set a DNS record value.
-        """
-        self.populate_cache()
-        self.api.set_dns_record('sub.domain.test.com', type='A', value='192.168.99.99')
-        self.assert_set_dns_record_calls()
-
-    @patch('time.sleep')
-    def test_set_dns_record_error_retry_and_succeed(self, sleep):
-        """
-        Test retry behaviour when setting a DNS record.  Succeed in the third attempt.
-        """
-        self.populate_cache()
-        self.api.client.make_version_creation_fail(2)
-        self.api.set_dns_record(
-            'sub.domain.test.com',
-            type='A', value='192.168.99.99'
-        )
-        self.assert_set_dns_record_calls(attempts=3)
-        self.assertEqual(sleep.mock_calls, [call(1), call(2)])
-
-    @patch('time.sleep')
-    def test_set_dns_record_timeout_retry_and_fail(self, sleep):
-        """
-        Test retry behaviour returns timeout exception. Fail all attempts.
-        """
-        self.api.client.make_version_creation_fail(10, timeout=True)
-        with self.assertRaises(TimeoutError):
-            self.api.set_dns_record(
-                'sub.domain.test.com',
-                type='A', value='192.168.99.99'
-            )
-        self.assertEqual(sleep.mock_calls, [call(1), call(2), call(4)])
-
-    @patch('time.sleep')
-    def test_set_dns_record_error_retry_and_fail(self, sleep):
-        """
-        Test retry behaviour when setting a DNS record.  Fail all attempts.
-        """
-        self.api.client.make_version_creation_fail(10)
-        with self.assertRaises(xmlrpc.client.Fault):
-            self.api.set_dns_record(
-                'sub.domain.test.com',
-                type='A', value='192.168.99.99'
-            )
-        self.assertEqual(sleep.mock_calls, [call(1), call(2), call(4)])
-
-    def test_remove_dns_record(self):
-        """
-        Test remove_dns_record().
-        """
-        self.populate_cache()
-        self.api.remove_dns_record('sub.domain.test.com')
-        self.assertEqual(self.api.client.domain.mock_calls, [
-            call.zone.version.new('TEST_GANDI_API_KEY', 9900),
-            call.zone.record.delete(
-                'TEST_GANDI_API_KEY', 9900, 1,
-                {'type': ['A', 'CNAME'], 'name': 'sub.domain'}
-            ),
-            call.zone.version.set('TEST_GANDI_API_KEY', 9900, 1),
-        ])
 
 
 class GandiV5TestCase(TestCase):
