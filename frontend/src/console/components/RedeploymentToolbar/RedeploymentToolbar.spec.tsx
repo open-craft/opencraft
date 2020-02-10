3cf4e1aae9b24e656603c6aba06f17bc6b27e822
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-02-10 10:42:21 -0300

    BB-2083 & BB-2084: Add deployment toolbar + modals, connect to backend
    Add deployment toolbar + modals, connect to backend, fix tests and lint code

--- frontend/src/console/components/RedeploymentToolbar/RedeploymentToolbar.spec.tsx
+++ frontend/src/console/components/RedeploymentToolbar/RedeploymentToolbar.spec.tsx
@@ -1,8 +1,74 @@
 import React from 'react';
 import { setupComponentForTesting } from "utils/testing";
 import { RedeploymentToolbar } from './RedeploymentToolbar';
+import { OpenEdXInstanceDeploymentStatusStatusEnum } from 'ocim-client';
 
-it('renders without crashing', () => {
-    const tree = setupComponentForTesting(<RedeploymentToolbar />).toJSON();
-    expect(tree).toMatchSnapshot();
+
+describe("RedeploymentToolbar Component", function() {
+  it('Correctly renders redeployment bar when deployment info isn\'t there', () => {
+      const tree = setupComponentForTesting(
+        <RedeploymentToolbar
+          deployment={undefined}
+          cancelRedeployment={() => {}}
+          performDeployment={() => {}}
+        />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+
+  it('Correctly renders redeployment bar when instance is being prepared', () => {
+      const tree = setupComponentForTesting(
+        <RedeploymentToolbar
+          deployment={{
+            status: OpenEdXInstanceDeploymentStatusStatusEnum.PREPARINGINSTANCE,
+            undeployedChanges: 0
+          }}
+          cancelRedeployment={() => {}}
+          performDeployment={() => {}}
+        />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+
+  it('Correctly renders redeployment bar when instance is up-to-date', () => {
+      const tree = setupComponentForTesting(
+        <RedeploymentToolbar
+          deployment={{
+            status: OpenEdXInstanceDeploymentStatusStatusEnum.UPTODATE,
+            undeployedChanges: 0
+          }}
+          cancelRedeployment={() => {}}
+          performDeployment={() => {}}
+        />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+
+  it('Correctly renders redeployment bar when instance has pending changes', () => {
+      const tree = setupComponentForTesting(
+        <RedeploymentToolbar
+          deployment={{
+            status: OpenEdXInstanceDeploymentStatusStatusEnum.PENDINGCHANGES,
+            undeployedChanges: 3
+          }}
+          cancelRedeployment={() => {}}
+          performDeployment={() => {}}
+        />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+
+  it('Correctly renders redeployment bar when instance is being deployed', () => {
+      const tree = setupComponentForTesting(
+        <RedeploymentToolbar
+          deployment={{
+            status: OpenEdXInstanceDeploymentStatusStatusEnum.DEPLOYING,
+            undeployedChanges: 0
+          }}
+          cancelRedeployment={() => {}}
+          performDeployment={() => {}}
+        />
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
 });
