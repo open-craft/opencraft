05dba886825f5cecf19e7aa026de8b55482bbb97
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-02-10 10:42:21 -0300

    BB-2083 & BB-2084: Add deployment toolbar + modals, connect to backend
    Add deployment toolbar + modals, connect to backend, fix tests and lint code

--- frontend/packages/api_client/src/models/index.ts
+++ frontend/packages/api_client/src/models/index.ts
@@ -5,6 +5,7 @@ export * from './InstanceReferenceDetailed';
 export * from './InstanceReferenceMinimal';
 export * from './OpenEdXAppServer';
 export * from './OpenEdXInstanceConfig';
+export * from './OpenEdXInstanceConfigUpdate';
 export * from './OpenEdXInstanceDeploymentCreate';
 export * from './OpenEdXInstanceDeploymentStatus';
 export * from './OpenStackServer';
---
+++ frontend/src/ui/components/CustomStatusPill/CustomStatusPill.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { WrappedMessage } from 'utils/intl';
+import { Tooltip, OverlayTrigger, Badge, Nav } from 'react-bootstrap';
+import { OpenEdXInstanceDeploymentStatusStatusEnum } from 'ocim-client';
+
+import messages from './displayMessages';
+import './styles.scss';
+
+interface Props {
+  loading: boolean;
+  redeploymentStatus: string;
+  cancelRedeployment: Function;
+}
+
+export const CustomStatusPill: React.FC<Props> = (props: Props) => {
+  let dotColor = 'grey';
+  let deploymentStatusText = 'unavailable';
+  let tooltipText = 'unavailableTooltip';
+
+  switch (props.redeploymentStatus) {
+    case OpenEdXInstanceDeploymentStatusStatusEnum.UPTODATE:
+      dotColor = '#1abb64';
+      deploymentStatusText = 'updatedDeployment';
+      tooltipText = 'updatedDeploymentTooltip';
+      break;
+    case OpenEdXInstanceDeploymentStatusStatusEnum.DEPLOYING:
+      dotColor = '#ff9b04';
+      deploymentStatusText = 'runningDeployment';
+      tooltipText = 'runningDeploymentTooltip';
+      break;
+    case OpenEdXInstanceDeploymentStatusStatusEnum.PREPARINGINSTANCE:
+      dotColor = '#ff9b04';
+      deploymentStatusText = 'preparingInstance';
+      tooltipText = 'preparingInstanceTooltip';
+      break;
+    case OpenEdXInstanceDeploymentStatusStatusEnum.PENDINGCHANGES:
+      dotColor = '#1abb64';
+      deploymentStatusText = 'pendingChanges';
+      tooltipText = 'pendingChangesTooltip';
+      break;
+    default:
+      // Default values already set up when instancing variables
+      break;
+  }
+
+  const tooltip = (
+    <Tooltip id="redeployment-status">
+      <WrappedMessage messages={messages} id={tooltipText} />
+    </Tooltip>
+  );
+
+  return (
+    <OverlayTrigger placement="right" overlay={tooltip}>
+      <Badge pill className="status-pill" variant="primary">
+        <span className="dot" style={{ backgroundColor: dotColor }} />
+        <div className="text">
+          <WrappedMessage id={deploymentStatusText} messages={messages} />
+        </div>
+        {props.redeploymentStatus ===
+          OpenEdXInstanceDeploymentStatusStatusEnum.DEPLOYING &&
+          !props.loading && (
+            <Nav
+              className="text cancel-deployment"
+              onClick={() => {
+                props.cancelRedeployment();
+              }}
+            >
+              <i className="fas fa-xs fa-times" />
+              <WrappedMessage id="cancelRedeployment" messages={messages} />
+            </Nav>
+          )}
+      </Badge>
+    </OverlayTrigger>
+  );
+};
