74d4f5efe865377603bab22aeda56a0ec244fdad
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-03-04 15:36:27 -0300

    Add more snapshot tests

--- frontend/src/console/components/ThemePreviewAndColors/ThemePreviewAndColors.spec.tsx
+++ frontend/src/console/components/ThemePreviewAndColors/ThemePreviewAndColors.spec.tsx
@@ -2,7 +2,73 @@ import React from 'react';
 import { setupComponentForTesting } from "utils/testing";
 import { ThemePreviewAndColors } from './ThemePreviewAndColors';
 
-it('renders without crashing', () => {
-    const tree = setupComponentForTesting(<ThemePreviewAndColors />).toJSON();
-    expect(tree).toMatchSnapshot();
+
+describe("Theme preview and colors page", function() {
+  it('renders without crashing', () => {
+      const tree = setupComponentForTesting(<ThemePreviewAndColors />).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+
+  it('Render theme settings page when instance has no theme set up.', () => {
+      const tree = setupComponentForTesting(
+        <ThemePreviewAndColors />,
+        {
+          console: {
+            loading: false,
+            activeInstance: {
+              data: {
+                id: 1,
+                instanceName: "test",
+                subdomain: "test",
+                draftThemeConfig: {}
+              },
+              deployment: {
+                status: "NO_STATUS",
+                undeployedChanges: 0
+              }
+            },
+            instances: [{
+              id: 1,
+              instanceName: "test",
+              subdomain: "test",
+            }]
+          }
+        }
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+
+  it('Render theme settings page with theme set up.', () => {
+      const tree = setupComponentForTesting(
+        <ThemePreviewAndColors />,
+        {
+          console: {
+            loading: false,
+            activeInstance: {
+              data: {
+                id: 1,
+                instanceName: "test",
+                subdomain: "test",
+                draftThemeConfig: {
+                  version: 1,
+                  mainColor: "#444444",
+                  linkColor: "#FFAAFF"
+                }
+              },
+              loading: [],
+              deployment: {
+                status: "NO_STATUS",
+                undeployedChanges: 0
+              }
+            },
+            instances: [{
+              id: 1,
+              instanceName: "test",
+              subdomain: "test",
+            }]
+          }
+        }
+      ).toJSON();
+      expect(tree).toMatchSnapshot();
+  });
 });
