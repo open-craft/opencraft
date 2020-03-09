0e617bab669b5b4012b64fd1ae0bebb8ac02163a
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-03-09 18:37:25 -0300

    Handle access and refresh token expiration Adds handlers to force refresh the page when an access token expires and logs users out when refresh token expires.

--- frontend/src/global/api.ts
+++ frontend/src/global/api.ts
@@ -1,5 +1,7 @@
 import { OCIM_API_BASE } from 'global/constants';
 import { V2Api as _V2Api, Configuration } from 'ocim-client';
+import { performLogout } from 'auth/actions';
+
 
 const config = new Configuration({
   basePath: `${OCIM_API_BASE}/api`,
@@ -8,7 +10,23 @@ const config = new Configuration({
     // The current version of the API client requires us to specify the word "Bearer"
     // The previous version did not.
     return authToken ? `Bearer ${authToken}` : '';
-  }
+  },
+  middleware: [
+    {
+      post: async (context) => {
+        if (context.response.status === 403) {
+          // Failed requests return 403 and mean that the access token is
+          // expired, so we trigger a page refresh.
+          window.location.reload(false);
+        } else if (context.response.status === 401) {
+          // If the refresh endpoint fails, it means that the refresh
+          // is expired, and the user is effectively logged out.
+          performLogout();
+        }
+        return context.response;
+      },
+    },
+  ]
 });
 
 export const V2Api = new _V2Api(config);
