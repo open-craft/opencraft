fbd9227e678aaa79b679e277fc1cbd5f5854350e
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-03-10 11:40:10 -0300

    Remove placeholder translations

--- frontend/src/registration/components/CongratulationsPage/displayMessages.ts
+++ frontend/src/registration/components/CongratulationsPage/displayMessages.ts
@@ -1,17 +1,17 @@
 const messages = {
   congrats: {
     defaultMessage: 'Congratulations!',
-    description: 'A description of the translatable string.'
+    description: ''
   },
   congratsMessage: {
     defaultMessage: 'You have successfully created your OpenCraft account.',
-    description: 'A description of the translatable string.'
+    description: ''
   },
   congratsMessage2: {
     defaultMessage:
       'We have sent you an email to verify your email address. Please follow ' +
       'the link that we emailed you to complete your application process.',
-    description: 'A description of the translatable string.'
+    description: ''
   }
 };
 
