79e15b0c1a461b4614484061ca1c129e8e8ecfe1
Giovanni Cimolin da Silva <giovannicimolin@gmail.com>
2020-04-23 19:05:01 -0300

    Improve styling and behavior of Hero page

--- frontend/src/ui/components/TextInputField/TextInputField.tsx
+++ frontend/src/ui/components/TextInputField/TextInputField.tsx
@@ -13,6 +13,7 @@ interface InputFieldProps {
   type?: string;
   isValid?: boolean;
   loading?: boolean;
+  reset?: Function;
 }
 
 export const TextInputField: React.SFC<InputFieldProps> = (
@@ -21,33 +22,51 @@ export const TextInputField: React.SFC<InputFieldProps> = (
   const hasHelpMessage = `${props.fieldName}Help` in props.messages;
 
   return (
-    <FormGroup>
-      <FormLabel>
-        <WrappedMessage id={props.fieldName} messages={props.messages} />
-      </FormLabel>
-      <FormControl
-        name={props.fieldName}
-        value={props.value}
-        disabled={props.loading}
-        onChange={props.onChange}
-        onBlur={props.onBlur}
-        type={props.type}
-        isInvalid={!!props.error}
-        isValid={props.isValid}
-      />
-      {props.error && (
-        <FormControl.Feedback type="invalid">
-          {props.error}
-        </FormControl.Feedback>
-      )}
-      <p>
-        {hasHelpMessage && (
-          <WrappedMessage
-            id={`${props.fieldName}Help`}
-            messages={props.messages}
-          />
+    <div className="text-input-container">
+      <FormGroup>
+        <FormLabel>
+          <WrappedMessage id={props.fieldName} messages={props.messages} />
+        </FormLabel>
+        <FormControl
+          name={props.fieldName}
+          value={props.value}
+          disabled={props.loading}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+          type={props.type}
+          isInvalid={!!props.error}
+          isValid={props.isValid}
+        />
+        {props.error && (
+          <FormControl.Feedback type="invalid">
+            {props.error}
+          </FormControl.Feedback>
         )}
-      </p>
-    </FormGroup>
+        {hasHelpMessage && (
+          <p>
+            <WrappedMessage
+              id={`${props.fieldName}Help`}
+              messages={props.messages}
+            />
+          </p>
+        )}
+      </FormGroup>
+
+      {props.reset !== undefined && (
+        <p>
+          <button
+            className="reset-default"
+            type="button"
+            onClick={() => {
+              if (props.reset !== undefined) {
+                props.reset();
+              }
+            }}
+          >
+            Reset
+          </button>
+        </p>
+      )}
+    </div>
   );
 };
