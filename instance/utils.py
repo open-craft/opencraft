38bff58364c9937e8da2197b97ea9435f2a8e9dd
Kshitij Sobti <kshitij@sobti.in>
2020-05-12 18:36:39 +0530

    Fix diffing function to include changes in static content

--- instance/utils.py
+++ instance/utils.py
@@ -177,6 +177,11 @@ def build_instance_config_diff(instance_config: 'BetaTestApplication'):
     if instance_config.draft_theme_config != instance.theme_config:
         original_config['theme_config'] = instance.theme_config
         new_config['theme_config'] = instance_config.draft_theme_config
+
+    if instance_config.draft_static_content_overrides != instance.static_content_overrides:
+        original_config['static_content_overrides'] = instance.static_content_overrides
+        new_config['static_content_overrides'] = instance_config.draft_static_content_overrides
+
     return list(diff(original_config, new_config))
 
 
