435d1f21069af81681c4a8661e7c34b23e40355f
Kshitij Sobti <kshitij@sobti.in>
2020-05-12 14:12:54 +0530

    Uneject

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const createProxyMiddleware = require('http-proxy-middleware');
 
 // @ts-ignore
 module.exports = function(app) {
