bf3b1952305f3bd8b5369aa583c2c10d414022f7
Kshitij Sobti <kshitij@opencraft.com>
2020-05-14 20:11:08 +0530

    Update frontend/src/registration/components/CongratulationsPage/displayMessages.ts
    Co-authored-by: Giovanni Cimolin da Silva <giovannicimolin@gmail.com>

--- frontend/src/registration/components/CongratulationsPage/displayMessages.ts
+++ frontend/src/registration/components/CongratulationsPage/displayMessages.ts
@@ -11,7 +11,7 @@ const messages = {
     defaultMessage:
       'We have sent you an email to verify your OpenCraft account email and ' +
       'your public contact email. If these are different email addresses, you ' +
-      'will need to verify <strong>both</strong> of them for your instance to' +
+      'will need to verify <strong>both</strong> of them for your instance to ' +
       'be activated. Please follow the link(s) that we emailed you to your email' +
       'account(s) complete your application process.',
     description: ''
