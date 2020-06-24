e41b98365ee1c996e905536055086b33c75d7599
Patrick Cockwell <patrick@opencraft.com>
2020-06-24 18:15:07 +0700

    BB-2627: First pass at playbooks and scripts to retrieve instance statistics from logs.opencraft.com and appservers

---
+++ instance/management/commands/instance_statistics_csv.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+#
+# OpenCraft -- tools to aid developing and hosting free software projects
+# Copyright (C) 2015-2019 OpenCraft <contact@opencraft.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""
+Instance app - Activity report management command
+"""
+
+# Imports #####################################################################
+
+from configparser import ConfigParser
+import csv
+from datetime import datetime
+import os
+import sys
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+
+from instance import ansible
+from instance.models.openedx_instance import OpenEdXInstance
+from registration.models import BetaTestApplication
+
+
+# Classes #####################################################################
+
+
+class Command(BaseCommand):
+    """
+    Logs_activity_csv management command class
+    """
+    help = (
+        'Generates a CSV containing basic activity information about all app servers'
+        ' (numbers for hits, distinct hits, users, and courses).'
+    )
+
+    LOGS_SERVER = 'logs.opencraft.com'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--qualified-domain',
+            '-q',
+            default=None,
+            help='The fully qualified domain name to filter results by',
+            required=True
+        )
+        parser.add_argument(
+            '--days',
+            '-d',
+            default=60,
+            help='The number of days that should be queried for statistics. Default is 60 days'
+        )
+        parser.add_argument(
+            '--out',
+            default=None,
+            help='Path to the output file of the new CSV. Leave blank to use stdout.'
+        )
+
+    def handle(self, *args, **options):
+        # Determine the stream to be used for outputting the CSV.
+        if options['out'] is None:
+            out = self.stdout
+        else:
+            try:
+                out = open(options['out'], 'w')
+            except PermissionError:
+                self.stderr.write(self.style.ERROR(
+                    'Permission denied while attempting to write file: {outfile}'.format(outfile=options['out'])
+                ))
+                sys.exit(1)
+
+        self.collect_instance_statistics(out, options['qualified_domain'], options['days'])
+
+    def get_instance_from_qualified_domain(self, qualified_domain):
+        try:
+            instance = OpenEdXInstance.objects.get(
+                Q(external_lms_domain=qualified_domain) | Q(internal_lms_domain=qualified_domain)
+            )
+        except OpenEdXInstance.DoesNotExist:
+            self.stderr.write(self.style.ERROR(
+                'No OpenEdXInstance exists with an external or internal domain of {qualified_domain}'.format(qualified_domain=qualified_domain)
+            ))
+            sys.exit(1)
+
+        # If there are no appservers for the instance, we should error out
+        if instance.appserver_set.first() is None:
+            self.stderr.write(self.style.ERROR(
+                'No OpenEdXAppServers have been created for the instance with external or internal domain of {qualified_domain}'.format(qualified_domain=qualified_domain)
+            ))
+            sys.exit(1)
+
+        return instance
+
+    def get_elasticsearch_statistics(playbook_output_dir, name_prefix, num_days):
+        inventory = '[apps]\n{server}'.format(server=self.LOGS_SERVER)
+        playbook_path = os.path.join(settings.SITE_ROOT, 'playbooks/collect_instance_statistics/collect_elasticsearch_data.yml')
+
+        def log_line(line):
+            """Helper to pass to capture_playbook_output()."""
+            self.stderr.write(self.style.SUCCESS(line))
+        log_line.info = log_line
+        log_line.error = log_line
+
+        # Launch the collect_elasticsearch_data playbook, which places a file into the `playbook_output_dir`
+        # on this host.
+        ansible.capture_playbook_output(
+            requirements_path=os.path.join(os.path.dirname(playbook_path), 'requirements.txt'),
+            inventory_str=inventory,
+            vars_str=(
+                'local_output_dir: {output_dir}\n'
+                'remote_output_filename: /tmp/activity_report\n'
+                'server_name_prefix: {server_name_prefix}\n'
+                'num_days: {num_days}'
+            ).format(
+                output_dir=playbook_output_dir,
+                server_name_prefix=name_prefix,
+                num_days=num_days
+            ),
+            playbook_path=playbook_path,
+            username=settings.OPENSTACK_SANDBOX_SSH_USERNAME,
+            logger_=log_line,
+        )
+
+    def get_appserver_statistics(playbook_output_dir, name_prefix, public_ip):
+        inventory = '[apps]\n{server}'.format(server=public_ip)
+        playbook_path = os.path.join(settings.SITE_ROOT, 'playbooks/collect_instance_statistics/collect_appserver_data.yml')
+
+        def log_line(line):
+            """Helper to pass to capture_playbook_output()."""
+            self.stderr.write(self.style.SUCCESS(line))
+        log_line.info = log_line
+        log_line.error = log_line
+
+        # Launch the collect_appserver_data playbook, which places a file into the `playbook_output_dir`
+        # on this host.
+        ansible.capture_playbook_output(
+            requirements_path=os.path.join(os.path.dirname(playbook_path), 'requirements.txt'),
+            inventory_str=inventory,
+            vars_str=(
+                'local_output_dir: {output_dir}\n'
+                'remote_output_filename: /tmp/activity_report\n'
+                'server_name_prefix: {server_name_prefix}'
+            ).format(
+                output_dir=playbook_output_dir,
+                server_name_prefix=name_prefix
+            ),
+            playbook_path=playbook_path,
+            username=settings.OPENSTACK_SANDBOX_SSH_USERNAME,
+            logger_=log_line,
+        )
+
+    def collect_instance_statistics(self, out, qualified_domain, num_days):  # pylint: disable=too-many-locals
+        """Generate the activity CSV."""
+        instance = self.get_instance_from_qualified_domain(qualified_domain)
+        appserver = instance.appserver_set.first()
+        name_prefix = appserver.server_name_prefix
+
+        self.stderr.write(self.style.SUCCESS('Running playbook...'))
+
+        with ansible.create_temp_dir() as playbook_output_dir:
+            get_elasticsearch_statistics(playbook_output_dir, name_prefix, num_days)
+            get_appserver_statistics(playbook_output_dir, name_prefix, appserver.server.public_ip)
+
+            csv_writer = csv.writer(out, quoting=csv.QUOTE_NONNUMERIC)
+            csv_writer.writerow([
+                'Fully Qualified Domain Name',
+                'Server Name Prefix',
+                'Name',
+                'Contact Email',
+                'Unique Hits',
+                'Total Hits',
+                'Total Courses',
+                'Total Users',
+                'Age (Days)'
+            ])
+
+            filenames = [os.path.join(playbook_output_dir, f) for f in os.listdir(playbook_output_dir)]
+            data = ConfigParser()
+            data.read(filenames)
+
+            try:
+                section = data[name_prefix]
+            except KeyError:
+                # Set the section to an empty dict
+                section = {}
+
+            instance_age = datetime.now(instance.created.tzinfo) - instance.created
+
+            try:
+                email = instance.betatestapplication_set.get().user.email
+            except BetaTestApplication.DoesNotExist:
+                email = 'N/A'
+
+            csv_writer.writerow([
+                instance.domain,
+                name_prefix,
+                instance.ref.name,
+                email,
+                section.get('unique_hits', 'N/A'),
+                section.get('total_hits', 'N/A'),
+                section.get('courses', 'N/A'),
+                section.get('users', 'N/A'),
+                instance_age.days
+            ])
+
+            self.stderr.write(self.style.SUCCESS('Done generating CSV output.'))
---
+++ playbooks/collect_instance_statistics/query_elasticsearch.py
@@ -0,0 +1,87 @@
+#!/edx/bin/python.edxapp
+# pylint: skip-file
+
+from ConfigParser import ConfigParser
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+import sys
+
+
+if __name__ == '__main__':
+
+    def hours_to_milliseconds(hours):
+        return hours * 60 * 60 * 1000
+
+    client = Elasticsearch()
+
+    # Grab the server_name_prefix passed to this script by the ansible playbook. This will be used as the
+    # ConfigParser "section" name for the output data. This is so the data can easily be associated with this app
+    # server by the `activity_csv` management command.
+    name_prefix = sys.argv[1]
+
+    # Get the number of days going back that we should search ES for
+    num_days = sys.argv[2]
+    now = int(datetime.utcnow().timestamp() * 1000)
+
+    # Query for the host matching the name_prefix
+    # Only query access log data, and ignore heartbeat or xqueue/get_queuelen requests
+    base_search = Search(using=client)
+        .query("match", host=name_prefix)
+        .query("match", source="/edx/var/log/nginx/access.log")
+        .exclude("match", request="heartbeat")
+        .exclude("match", request="xqueue/get_queuelen")
+
+    # Aggregate the unique hits by proxy_ip
+    base_search.aggs.bucket("unique_hits", "terms", size=10000, field="proxy_ip.keyword")
+
+    unique_ips = set()
+    total_hits = 0
+
+    # Start num_days ago, and query for every 6 hour timeframe since
+    for start_time in range(now - hours_to_milliseconds(num_days * 24), now, hours_to_milliseconds(6)):
+        # Don't search beyond right now
+        end_time = min(start_time + hours_to_milliseconds(6), now)
+
+        # Search for the current 6 hour window
+        time_filter = {
+            "@timestamp": {
+                "gte": start_time,
+                "lt": end_time,
+                "format": "epoch_millis"
+            }
+        }
+        es_search = base_search.filter("range", **time_filter)
+
+        # Python slice syntax is used to set the `from` and `size`
+        # parameters for the search, which we want set to 0
+        response = es_search[0:0]
+
+        for bucket in response["aggregations"]["unique_hits"]["buckets"]:
+            ip = bucket["key"]
+
+            # Ignore requests without a proxy_ip
+            if ip == "-":
+                continue
+
+            unique_ips.add(ip)
+            total_hits += bucket["doc_count"]
+
+    stats = {
+        'unique_hits': len(unique_ips),
+        'total_hits': total_hits,
+    }
+
+    # Build the ConfigParser data.
+    config = ConfigParser()
+    config.add_section(public_ip)
+    for key, value in stats.items():
+        config.set(public_ip, key, value)
+
+    # Output the data in ConfigParser format to stdout and to a file.
+    config.write(sys.stdout)
+    if len(sys.argv) > 3:
+        filename = sys.argv[3]
+        print >> sys.stderr, 'Writing to file passed via parameter: {filename}'.format(filename=filename)
+        with open(filename, 'w') as output_file:
+            config.write(output_file)
