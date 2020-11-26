7894b3de88a96cc450f2ff7f35c8ce9a2dd2ff51
0x29a <demid@opencraft.com>
2020-11-26 16:06:06 +0100

    Change configuration and release versions

--- instance/tests/integration/factories/instance.py
+++ instance/tests/integration/factories/instance.py
@@ -79,8 +79,8 @@ class OpenEdXInstanceFactory(DjangoModelFactory):
     # or a release candidate tag will work.  We point both the edx-platform and the configuration
     # versions to the branch "integration" in our own forks.  These branches are based on the
     # corresponding openedx_release versions from upstream, but can contain custom modifications.
-    openedx_release = 'open-release/juniper.3'
+    openedx_release = 'open-release/koa.master'
     configuration_source_repo_url = 'https://github.com/open-craft/configuration.git'
-    configuration_version = 'integration-juniper'
+    configuration_version = 'integration-koa'
     edx_platform_repository_url = 'https://github.com/open-craft/edx-platform.git'
-    edx_platform_commit = 'opencraft-release/juniper.3'
+    edx_platform_commit = 'opencraft-release/koa.master'
