508a30e341aa99ec92dc175491b157d7acca7e69
Kaustav Banerjee <kaustav@opencraft.com>
2021-11-10 21:03:11 +0530

    fix: Fixed failing unit test

--- registration/tests/test_approval.py
+++ registration/tests/test_approval.py
@@ -23,6 +23,7 @@ Tests for the betatest approval helper functions
 # Imports #####################################################################
 
 from unittest import mock
+from datetime import datetime
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -30,6 +31,7 @@ from django.test import TestCase
 from instance.models.appserver import AppServer
 from registration.approval import ApplicationNotReady, accept_application, on_appserver_spawned
 from registration.models import BetaTestApplication
+from userprofile.models import UserProfile
 
 
 # Test cases ##################################################################
@@ -126,9 +128,21 @@ class ApprovalTestCase(TestCase):
         Check if the the first Appserver is correctly activated even when
         spawned manually
         """
+        user = get_user_model().objects.create_user(username='test', email='test@example.com')
+
+        UserProfile.objects.create(
+            user=user,
+            full_name="Test user 1",
+            accepted_privacy_policy=datetime.now(),
+            accept_domain_condition=True,
+            subscribe_to_updates=True,
+        )
+
+        user.refresh_from_db()
+
         appserver = mock.Mock(status=AppServer.Status.Running)
         instance = mock.Mock(first_activated=None)
-        application = mock.Mock(status=BetaTestApplication.PENDING)
+        application = mock.Mock(user=user, status=BetaTestApplication.PENDING)
 
         application.instance = instance
         instance.betatestapplication_set.first = lambda: application
